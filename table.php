8e7d6434663ebf44405d41692a48a05e71d79199
André Rodrigues (Xor) <xor2017@outlook.com>
2019-06-24 20:01:36 -0300

    table file

---
+++ table.php
@@ -0,0 +1,23 @@
+<!-- Datatable CSS -->
+<link href='//cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css' rel='stylesheet' type='text/css'>
+
+<!-- jQuery Library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+<!-- Datatable JS -->
+<script src="//cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
+
+<!-- Table -->
+<table id='empTable' class='display dataTable'>
+
+  <thead>
+    <tr>
+      <th>Employee name</th>
+      <th>Email</th>
+      <th>Gender</th>
+      <th>Salary</th>
+      <th>City</th>
+    </tr>
+  </thead>
+
+</table>
