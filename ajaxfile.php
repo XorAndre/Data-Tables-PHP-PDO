b04179105233f544e6ee1ddc018d37f1802b4645
André Rodrigues (Xor) <xor2017@outlook.com>
2019-06-24 20:04:05 -0300

    Query file ok

---
+++ ajaxfile.php
@@ -0,0 +1,73 @@
+<?php
+include 'config.php';
+
+## Read value
+$draw = $_POST['draw'];
+$row = $_POST['start'];
+$rowperpage = $_POST['length']; // Rows display per page
+$columnIndex = $_POST['order'][0]['column']; // Column index
+$columnName = $_POST['columns'][$columnIndex]['data']; // Column name
+$columnSortOrder = $_POST['order'][0]['dir']; // asc or desc
+$searchValue = $_POST['search']['value']; // Search value
+
+$searchArray = array();
+
+## Search 
+$searchQuery = " ";
+if($searchValue != ''){
+   $searchQuery = " AND (emp_name LIKE :emp_name or 
+        email LIKE :email OR 
+        city LIKE :city ) ";
+   $searchArray = array( 
+        'emp_name'=>"%$searchValue%", 
+        'email'=>'%$email%',
+        'city'=>'%$searchValue%'
+   );
+}
+
+## Total number of records without filtering
+$stmt = $conn->prepare("SELECT COUNT(*) AS allcount FROM employee ");
+$stmt->execute();
+$records = $stmt->fetch();
+$totalRecords = $records['allcount'];
+
+## Total number of records with filtering
+$stmt = $conn->prepare("SELECT COUNT(*) AS allcount FROM employee WHERE 1 ".$searchQuery);
+$stmt->execute($searchArray);
+$records = $stmt->fetch();
+$totalRecordwithFilter = $records['allcount'];
+
+## Fetch records
+$stmt = $conn->prepare("SELECT * FROM employee WHERE 1 ".$searchQuery." ORDER BY ".$columnName." ".$columnSortOrder." LIMIT :limit,:offset");
+
+// Bind values
+foreach($searchArray as $key=>$search){
+   $stmt->bindValue(':'.$key, $search,PDO::PARAM_STR);
+}
+
+$stmt->bindValue(':limit', (int)$row, PDO::PARAM_INT);
+$stmt->bindValue(':offset', (int)$rowperpage, PDO::PARAM_INT);
+$stmt->execute();
+$empRecords = $stmt->fetchAll();
+
+$data = array();
+
+foreach($empRecords as $row){
+   $data[] = array(
+      "emp_name"=>$row['emp_name'],
+      "email"=>$row['email'],
+      "gender"=>$row['gender'],
+      "salary"=>$row['salary'],
+      "city"=>$row['city']
+   );
+}
+
+## Response
+$response = array(
+   "draw" => intval($draw),
+   "iTotalRecords" => $totalRecords,
+   "iTotalDisplayRecords" => $totalRecordwithFilter,
+   "aaData" => $data
+);
+
+echo json_encode($response);
